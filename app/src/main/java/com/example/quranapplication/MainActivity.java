d2d14a4d4fa2cbb814bb4d8be4df3e232c3252ac
Aliza Tariq <alizatariq1122@gmail.com>
2023-02-13 00:07:29 +0500

    QuranApplication

--- app/src/main/java/com/example/quranapplication/MainActivity.java
+++ app/src/main/java/com/example/quranapplication/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.quranapplication;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -12,6 +18,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -28,87 +35,155 @@ public class MainActivity extends AppCompatActivity {
     public static List<QuranModel> objectList = new ArrayList<>();
 
     Button btn1;
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    ActionBarDrawerToggle toggle;
+
+
+    @Override
+    public void onBackPressed(){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+            Toast.makeText(getApplicationContext(),"Start",Toast.LENGTH_LONG).show();
+
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(),"End",Toast.LENGTH_LONG).show();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        t1=findViewById(R.id.text3);
-        btn1=findViewById(R.id.btn1);
 
-        String jsondata="";
-        try {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-            InputStream is = getAssets().open("QuranMetaData.json");
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-            jsondata = new String(buffer, "UTF-8");
-            System.out.println(jsondata);
-           // t1.setText(jsondata.toString());
-           // t1.setText("qwerty");
-        }
-        catch (Exception ex)
-        {
-            System.out.println("Exception ocuur");
-        }
+        navigationView=findViewById(R.id.nav_view);
+        drawerLayout=findViewById(R.id.drawer);
 
-        Gson gson = new Gson();
-        Type listType = new TypeToken<List<QuranModel>>(){}.getType();
-        objectList = gson.fromJson(jsondata, listType);
+        toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
 
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem)
+            {
+                switch (menuItem.getItemId())
+                {
+                    case R.id.nav_book :
+                        Toast.makeText(getApplicationContext(),"Retur is Clicked",Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(MainActivity.this, BookActivity.class);
+                        startActivity(intent);
+                        //drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
 
-        //MySingleton s=new MySingleton(this);
-        //List<QuranModel> objectList=s.getInstance().getMyList();
+                    case R.id.nav_return :
+                        Toast.makeText(getApplicationContext(),"Retur is Clicked",Toast.LENGTH_LONG).show();
+                        //drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
 
+                    case R.id.nav_laptop :
+                        Toast.makeText(getApplicationContext(),"Laptop is clicked",Toast.LENGTH_LONG).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
 
-//        for(int i=0;i<10;i++)
-//        {
-//            t1.setText(t1.getText()+objectList.get(i).getSurah_name());
+                    case R.id.nav_voice :
+                        Toast.makeText(getApplicationContext(),"Voice is clicked",Toast.LENGTH_LONG).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+
+                    case R.id.nav_chrome_reader :
+                        Toast.makeText(getApplicationContext(),"Chrome Reader is clicked",Toast.LENGTH_LONG).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                }
+
+                return true;
+            }
+        });
+
+        //t1=findViewById(R.id.text3);
+//        btn1=findViewById(R.id.btn1);
+//
+//        String jsondata="";
+//        try {
+//
+//            InputStream is = getAssets().open("QuranMetaData.json");
+//            int size = is.available();
+//            byte[] buffer = new byte[size];
+//            is.read(buffer);
+//            is.close();
+//            jsondata = new String(buffer, "UTF-8");
+//            System.out.println(jsondata);
+//           // t1.setText(jsondata.toString());
+//           // t1.setText("qwerty");
 //        }
-      //  t1.setText(getParahDetail(objectList,1));
-        list=findViewById(R.id.mylistView1);
-        arrayList=JsonHelper.getSurahNames(objectList,30);
-        ArrayAdapter ad=new ArrayAdapter(this,
-                android.R.layout.simple_list_item_1,arrayList);
-        list.setAdapter(ad);
-
-
-
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SurahActivity.class);
-                int number=123456;
-
-                //intent.putExtra("WelcomeMessage","السلام علیکم");
-                //intent.putExtra("nmbr",number);
-               // intent.putExtra("array_list",objectList);
-                //intent.putExtra("objectList", String.valueOf( objectList));
-                startActivity(intent);
-
-            }
-        });
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String item = (String) parent.getItemAtPosition(position);
-                Toast.makeText(MainActivity.this,item +" is Clicked",Toast.LENGTH_LONG).show();
-               String []obj1=item.split(". ");
-                Intent intent = new Intent(MainActivity.this, AyatActivity.class);
-                int number=123456;
-
-                intent.putExtra("WelcomeMessage","السلام علیکم");
-                intent.putExtra("nmbr",number);
-                intent.putExtra("Parah",Integer.parseInt(obj1[0]));
-                intent.putExtra("ParahName",obj1[1]);
-                intent.putExtra("array_list",JsonHelper.getAyatDetail(objectList,Integer.parseInt(obj1[0])));
-                startActivity(intent);
-
-            }
-        });
+//        catch (Exception ex)
+//        {
+//            System.out.println("Exception ocuur");
+//        }
+//
+//        Gson gson = new Gson();
+//        Type listType = new TypeToken<List<QuranModel>>(){}.getType();
+//        objectList = gson.fromJson(jsondata, listType);
+//
+//
+//
+//        //MySingleton s=new MySingleton(this);
+//        //List<QuranModel> objectList=s.getInstance().getMyList();
+//
+//
+////        for(int i=0;i<10;i++)
+////        {
+////            t1.setText(t1.getText()+objectList.get(i).getSurah_name());
+////        }
+//      //  t1.setText(getParahDetail(objectList,1));
+//        list=findViewById(R.id.mylistView1);
+//        arrayList=JsonHelper.getSurahNames(objectList,30);
+//        ArrayAdapter ad=new ArrayAdapter(this,
+//                android.R.layout.simple_list_item_1,arrayList);
+//        list.setAdapter(ad);
+//
+//
+//
+//        btn1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, SurahActivity.class);
+//                int number=123456;
+//
+//                //intent.putExtra("WelcomeMessage","السلام علیکم");
+//                //intent.putExtra("nmbr",number);
+//               // intent.putExtra("array_list",objectList);
+//                //intent.putExtra("objectList", String.valueOf( objectList));
+//                startActivity(intent);
+//
+//            }
+//        });
+//        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                String item = (String) parent.getItemAtPosition(position);
+//                Toast.makeText(MainActivity.this,item +" is Clicked",Toast.LENGTH_LONG).show();
+//               String []obj1=item.split(". ");
+//                Intent intent = new Intent(MainActivity.this, AyatActivity.class);
+//                int number=123456;
+//
+//                intent.putExtra("WelcomeMessage","السلام علیکم");
+//                intent.putExtra("nmbr",number);
+//                intent.putExtra("Parah",Integer.parseInt(obj1[0]));
+//                intent.putExtra("ParahName",obj1[1]);
+//                intent.putExtra("array_list",JsonHelper.getAyatDetail(objectList,Integer.parseInt(obj1[0])));
+//                startActivity(intent);
+//
+//            }
+//        });
 
     }
 
