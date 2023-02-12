c142523d2abd807fd1672691c11a4e7e2a504781
Aliza Tariq <alizatariq1122@gmail.com>
2023-02-12 13:25:44 +0500

    QuranApplication

--- app/src/main/java/com/example/quranapplication/DBHelper.java
+++ app/src/main/java/com/example/quranapplication/DBHelper.java
@@ -1,4 +1,40 @@
 package com.example.quranapplication;
 
 public class DBHelper {
+    public static final String text = "text";
+    public static final String numberInSurah = "numberInSurah";
+    public static final String juz = "juz";
+    public static final String manzil = "manzil";
+    public static final String page = "page";
+    public static final String ruku = "ruku";
+    public static final String hizbQuarter = "hizbQuarter";
+    public static final String sajda = "sajda";
+
+
+    public static final String surah_number = "surah_number";
+    public static final String surah_name = "surah_name";
+    public static final String englishName = "englishName";
+    public static final String englishNameTranslation = "englishNameTranslation";
+    public static final String revelationType = "revelationType";
+
+    public static final String sajdaId= "sajda.id";
+    public static final String sajdaRecommended= "sajda.recommended";
+    public static final String sajdaObligatory= "sajda.obligatory";
+
+
+    public static final String UrduTranslation = "UrduTranslation";
+    public static final String UrduTafseer = "UrduTafseer";
+    public static final String EnglishTranslation = "EnglishTranslation";
+    public static final String Englishtafseer = "Englishtafseer";
+    public static final String SindhiTranslation = "SindhiTranslation";
+    public static final String SindhiTafseer = "SindhiTafseer";
+    public static final String HindiTranslation = "HindiTranslation";
+    public static final String HindiTafseer = "HindiTafseer";
+    public static final String PushtoTransation = "PushtoTransation";
+    public static final String PushtoTafseer = "PushtoTafseer";
+
+
+    public static final String QURAN_TABLE = "QuranMetaData";
+
+
 }
