1c58193e6008abd10c55be3d0f81e905e08cf0a8
Aliza Tariq <alizatariq1122@gmail.com>
2023-02-13 21:10:51 +0500

    QuranApplication

--- app/src/main/java/com/example/quranapplication/MainActivity.java
+++ app/src/main/java/com/example/quranapplication/MainActivity.java
@@ -146,57 +146,6 @@ public class MainActivity extends AppCompatActivity {
         Type listType = new TypeToken<List<QuranModel>>(){}.getType();
         objectList = gson.fromJson(jsondata, listType);
 
-//
-//
-//        //MySingleton s=new MySingleton(this);
-//        //List<QuranModel> objectList=s.getInstance().getMyList();
-//
-//
-////        for(int i=0;i<10;i++)
-////        {
-////            t1.setText(t1.getText()+objectList.get(i).getSurah_name());
-////        }
-//      //  t1.setText(getParahDetail(objectList,1));
-//        list=findViewById(R.id.mylistView1);
-//        arrayList=JsonHelper.getSurahNames(objectList,30);
-//        ArrayAdapter ad=new ArrayAdapter(this,
-//                android.R.layout.simple_list_item_1,arrayList);
-//        list.setAdapter(ad);
-//
-//
-//
-//        btn1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(MainActivity.this, SurahActivity.class);
-//                int number=123456;
-//
-//                //intent.putExtra("WelcomeMessage","السلام علیکم");
-//                //intent.putExtra("nmbr",number);
-//               // intent.putExtra("array_list",objectList);
-//                //intent.putExtra("objectList", String.valueOf( objectList));
-//                startActivity(intent);
-//
-//            }
-//        });
-//        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                String item = (String) parent.getItemAtPosition(position);
-//                Toast.makeText(MainActivity.this,item +" is Clicked",Toast.LENGTH_LONG).show();
-//               String []obj1=item.split(". ");
-//                Intent intent = new Intent(MainActivity.this, AyatActivity.class);
-//                int number=123456;
-//
-//                intent.putExtra("WelcomeMessage","السلام علیکم");
-//                intent.putExtra("nmbr",number);
-//                intent.putExtra("Parah",Integer.parseInt(obj1[0]));
-//                intent.putExtra("ParahName",obj1[1]);
-//                intent.putExtra("array_list",JsonHelper.getAyatDetail(objectList,Integer.parseInt(obj1[0])));
-//                startActivity(intent);
-//
-//            }
-//        });
 
     }
     public void openWebPage(View view) {
